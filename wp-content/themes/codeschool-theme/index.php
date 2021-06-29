56b08912fdc4d07297b3c9e84b2d787c8c1c35c7
Sanjib <sanjib12sinha@gmail.com>
2021-06-29 17:25:43 +0530

    arrays

--- wp-content/themes/codeschool-theme/index.php
+++ wp-content/themes/codeschool-theme/index.php
@@ -9,4 +9,14 @@
 <?php bloginfo('description'); ?>
 
 </p>
-<h1>Something</h1>
+<h1>Names</h1>
+
+<?php
+
+$names = array('Mona', 'Babu', 'Pota', 'Gopal');
+$a = 0;
+while ($a < count($names)) {
+    # code...
+    echo "<li>$names[$a]</li>";
+    $a++;
+}
